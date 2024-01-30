d92110115af54ee65eb78f264f8f9493c97874d9
ii02735 <yadallee.bilaal@outlook.fr>
2024-01-30 18:23:37 +0100

    OPCache is not necessary

--- deploy.review.sh
+++ deploy.review.sh
@@ -4,4 +4,3 @@ composer clearcache && composer install
 php7.4 bin/console cache:clear
 php7.4 bin/console doctrine:schema:update --dump-sql --force
 yarn install --frozen-lockfile && yarn build
-cachetool opcache:reset
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ php7.4 bin/console cache:clear
 php7.4 bin/console doctrine:schema:update --dump-sql --force
 
 yarn install --frozen-lockfile && yarn build
-cachetool opcache:reset
 
 git config --local user.email "gfrydz2@gmail.com"
 git config --local user.name "GeoDaz"
