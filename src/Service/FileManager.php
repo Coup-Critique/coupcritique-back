72dff936285a83c39e84ccae634bee2c4e54f903
ii02735 <yadallee.bilaal@outlook.fr>
2024-02-26 11:50:29 +0100

    feat: command for centred cropping of user images

--- src/Service/FileManager.php
+++ src/Service/FileManager.php
@@ -44,12 +44,16 @@ class FileManager
 	{
 		$fullPath = "{$this->publicPath}/$filePath";
 		$resized_image = $this->imageManager->resizeImage($fullPath, $size);
-		imagesavealpha($resized_image, True);
-		if (getimagesize($fullPath)["mime"] == 'image/png') {
-			imagepng($resized_image, $fullPath);
-		} else {
-			imagejpeg($resized_image, $fullPath);
-		}
+		imageSaveAlpha($resized_image, True);
+		$this->imageManager->saveImage($resized_image, $fullPath);
+		imageDestroy($resized_image);
+	}
+
+	public function imageCropCenter(string $filePath): void {
+		$fullPath = "{$this->publicPath}/$filePath";
+		$image = $this->imageManager->cropCenter($this->imageManager->createImageFromPath($fullPath));
+		$this->imageManager->saveImage($image, $fullPath);
+		imageDestroy($image);
 	}
 
 	private function createDirIfNotExists($dirName): void
