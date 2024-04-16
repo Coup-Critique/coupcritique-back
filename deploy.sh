93db41682c5f75b67510cfdf574a90235a15b92d
GeoDaz <gfrydz2@gmail.com>
2024-04-16 09:18:31 +0200

    in sf6, we should use --complete on d:s:u

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 composer clearcache && composer install
 php bin/console cache:clear
-php bin/console doctrine:schema:update --dump-sql --force
+php bin/console doctrine:schema:update --dump-sql --force --complete
